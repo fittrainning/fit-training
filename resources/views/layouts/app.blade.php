64a48af5fcc7544b94ba7a3a9b1f18edc6e6fb91
Kratoz98 <criscaanca@gmail.com>
2020-10-03 19:00:20 -0500

    menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,40 +32,43 @@
             <nav class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
                 <li class="nav dropdown justify-content-end">
                     <!--Esta etiqueta contiene el icono de inicio de sesion-->
-                    <a class="nav-link dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a href="{{ route('home') }}" class="nav-link dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <!--Esta determina el boton-->
                         <img id="inicio" src="img/porfile.png" alt="porfile" width="80px">
                         <!--Esta etiqueta define la imagen de inicio de sesion-->
                     </a>
-                    <div id="fon" class="dropdown-menu dropdown-menu-right">
+                    
                         <!-- Authentication Links -->
                         @guest
-                        <div class="top-right links">
-                            <a class="dropdown-item btn" type="button" href="{{ route('login') }}">{{ __('Iniciar Sesion') }}</a>
-                            @if (Route::has('register'))
-                                <a class="dropdown-item btn" type="button" href="{{ route('register') }}">{{ __('Registrate') }}</a>
-                            @endif
-                        <div>
+                            <div id="fon" class="dropdown-menu dropdown-menu-right">
+                                <div class="top-right links">
+                                    <a class="dropdown-item btn" type="button" href="{{ route('login') }}">{{ __('Iniciar Sesion') }}</a>
+                                    @if (Route::has('register'))
+                                        <a class="dropdown-item btn" type="button" href="{{ route('register') }}">{{ __('Registrate') }}</a>
+                                    @endif
+                                <div>
+                            </div>
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                        {{ __('Cerrar Sesion') }}
+                            <div id="fon" class="dropdown-menu dropdown-menu-right">
+                                <div class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }} <span class="caret"></span>
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                            {{ __('Cerrar Sesion') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
                                 </div>
-                            </li>
+                            </div>
                         @endguest
-                    </div>
                 </li>
             </nav>
         </div>
