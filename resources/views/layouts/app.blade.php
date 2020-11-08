1c220707aa41e5bd08a2a17098ac161476df4e9c
Kratoz98 <ccanzola3@misena.edu.co>
2020-11-07 19:03:12 -0500

    diseño 4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,6 +59,7 @@
                                                 document.getElementById('logout-form').submit();">
                                     {{ __('Cerrar Sesion') }}
                                 </a>
+
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
                                 </form>
@@ -68,31 +69,31 @@
                 </li>
             </nav>
         </div>
+
         <div class="row">
             <div class="wrapper">
                 <!-- Sidebar  -->
                 <nav id="sidebar">
-                    <ul class="list-unstyled components" id="barra"> 
-                        <li>
-                            <a class="nav-link" href="En_construccion.php">
-                                <p id="texma"><img src="../img/bioquimica.png" alt="gestion_ficha" width="35px">Caracterizacion</p>
+                    <ul class="list-unstyled components">
+                    <li>
+                            <a class="nav-link" href="cargar_ficha.php" id="barra">
+                                <p id="texma"><img src="../img/clipboard109-01.png" alt="gestion_ficha" width="35px">&nbsp;Gestion Ficha</p>
                             </a>
                         </li>
                         <li>
-                            <a class="nav-link" href="Bateria de test.php">
-                                <p id="texma"><img src="../img/entrenamiento.png" alt="gestion_usuario" width="35px" >Bateria de Test</p>
+                            <a class="nav-link" href="cargar_usuario.php">
+                                <p id="texma"><img src="../img/impreso-1.png" alt="gestion_usuario" width="35px">Gestion Usuario</p>
                             </a>
                         </li>
                         <li>
-                            <a class="nav-link" href="En_construccion.php">
-                                <p id="texma"><img src="../img/Agenda.png" width="35px">    Agenda</p>
+                            <a class="nav-link" href="Di_construccion.php">
+                                <p id="texma"><img src="../img/multitudb.png" alt="entrenadores" width="35px">&nbsp;Entrenadores</p>
                             </a>
                         </li>
                         <li>
-                            <a class="nav-link" href="En_construccion.php">
-                                <p id="texma"><img src="../img/estadistica.png" alt="estadisticas" width="35px">Estadisticas</p>
+                            <a class="nav-link" href="Di_construccion.php">
+                                <p id="texma"><img src="../img/estadistica.png" alt="estadisticas" width="35px">&nbsp;Estadisticas</p>
                             </a>
-                        
                         </li>
                     </ul>
                 </nav>
@@ -104,21 +105,20 @@
                     </button>
         
         
-                    @yield('content')
+                    <main class="py-4">
+                        @yield('content')
+                    </main>
         
         
                 </div>
             </div>
         </div>
     </div>
-    
     <!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <!-- Popper.JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <!-- Bootstrap JS -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-
+    
     <script type="text/javascript">
         $(document).ready(function () {
             $('#sidebarCollapse').on('click', function () {
