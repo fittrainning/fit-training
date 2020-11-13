8ac5e966350af3b4005d2055db92ba370619d192
Kratoz98 <ccanzola3@misena.edu.co>
2020-11-12 23:09:15 -0500

    vista Mensajeria 1

--- resources/views/mensajeria.blade.php
+++ resources/views/mensajeria.blade.php
@@ -5,15 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
             <div id="cont"><br><br>
-                    
-                <!--Icono de cerrar-->
-                <div class="row bm" id="cerrar">
-                    <p><input type="image" name="botondeenvio" src="../img/equis.png"  width="40px"></p>
-                </div><br><br><br>
-                
-                <!--FORMULARIO-->
-                <div class="form">
-                    
+                <div class="row">
+                    <div class="col-12">
+                        Mensajeria
+                    </div>
                 </div>
             </div><br>
         </div>
