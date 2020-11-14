35a40831be0ef00c25d9f358fe619d9acc643463
Kratoz98 <ccanzola3@misena.edu.co>
2020-11-14 11:30:33 -0500

    clases y docificacion

--- resources/views/entrenador/bateriat.blade.php
+++ resources/views/entrenador/bateriat.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-12">
+        <div class="col-11 col-sm-11 col-md-11 col-lg-11 col-xl-11">
             <div id="cont"><br><br>
 
                 <table class="table table-borderless">
@@ -65,5 +65,6 @@
                 </a><br><br>
             </div>
         </div>
+        <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
     </div>
 @endsection
