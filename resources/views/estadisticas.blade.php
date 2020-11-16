731932ffa61622f3e9f79bec595786de90239b2b
Kratoz98 <ccanzola3@misena.edu.co>
2020-11-16 00:17:04 -0500

    estadistica 1

--- resources/views/estadisticas.blade.php
+++ resources/views/estadisticas.blade.php
@@ -28,7 +28,24 @@
                             <div id="econ">
                                 <div id="ecc1">deporte</div>
                                 <div id="ecc2">ficha</div>
-                                <div id="ecc3">plan</div>
+                                <div id="ecc3">
+                                    <div class="row">
+                                        <div class="col-6"></div>
+                                        <div class="col-6">
+                                            <div class="input-group mb-3">
+                                                <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                                                <div class="input-group-append">
+                                                  <button class="btn btn-outline-secondary" type="button">Button</button>
+                                                </div>
+                                              </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-12">
+                                            plan
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
