fdcc59687d8cd06d2150ec115e9930386dc83b94
fittrainning <67293322+fittrainning@users.noreply.github.com>
2020-11-16 17:52:21 -0500

    ff

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.app' )
 
 @section('content')
-<div class="container">
+<div class="container" >
     <br><br>
     <div class="row justify-content-center">
         <div class="col-md-8">
