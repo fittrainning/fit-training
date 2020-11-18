c9d47828f0ed61a1cb1140cbfbe6d96baeaa607c
Kratoz98 <ccanzola3@misena.edu.co>
2020-11-18 12:48:23 -0500

    deportes

---
+++ resources/views/entrenador/deporte.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-11 col-sm-11 col-md-11 col-lg-11 col-xl-11">
+        <div id="cont"><br><br>
+            <div class="row">
+                <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+                <div class="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10" id="colo">
+                    <div class="row">
+                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" id="cenn">
+                            <h4>Añadir Deporte</h4>
+                        </div>
+                    </div>
+                    <hr id="separa">
+                    <div class="row">
+                        <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+                        <div class="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10">
+                            <form action="">
+                                
+                            </form>
+                        </div>
+                        <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+                    </div>
+                </div>
+                <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+            </div>
+        </div><br>
+    </div>
+    <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+</div>
+@endsection
