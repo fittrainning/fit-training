535816f1be602de4c2171e5b4a5228c1c173ba99
Kratoz98 <ccanzola3@misena.edu.co>
2020-11-17 19:38:41 -0500

    vistas 2.0

--- resources/views/director/entrenador.blade.php
+++ resources/views/director/entrenador.blade.php
@@ -10,11 +10,22 @@
                 </div>
                 <br>
                 <br>
-                <div class="d-flex justify-content-around">
-                    <table>
-                        
-                    </table>
-                </div>
+                <table id="tabla">
+                    <tr id="cabe">
+                        <th>Documento</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Ficha</th>
+                        <th>Plan</th>
+                    </tr>
+                    <tr id="cuer">
+                        <td>{{ Auth::user()->Usu_id }}</td>
+                        <td>{{ Auth::user()->name }}</td>
+                        <td>{{ Auth::user()->Usu_apellidos }}</td>
+                        <td>1907163</td>
+                        <td><a href="{{ route('plan') }}"><input id="inn" type="button" value="ver"></a></td>
+                    </tr>
+                </table>
                 <br><br>
             </div>
         </div>
