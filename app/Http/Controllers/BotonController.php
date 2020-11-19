cf92fc6074fc2fe6a1c3d3d33ba6750cb7f1573e
PaulaRomero_66 <pjromero67@misena.edu.co>
2020-11-18 22:32:14 -0500

    mcr

---
+++ app/Http/Controllers/BotonController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\boton;
+use Illuminate\Http\Request;
+
+class BotonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\boton  $boton
+     * @return \Illuminate\Http\Response
+     */
+    public function show(boton $boton)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\boton  $boton
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(boton $boton)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\boton  $boton
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, boton $boton)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\boton  $boton
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(boton $boton)
+    {
+        //
+    }
+}
