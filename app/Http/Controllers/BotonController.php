a04adcdccc9b4972e332986592c8425d28f239ac
PaulaRomero_66 <pjromero67@misena.edu.co>
2020-11-21 13:18:33 -0500

    boton

--- app/Http/Controllers/BotonController.php
+++ app/Http/Controllers/BotonController.php
@@ -14,7 +14,8 @@ class BotonController extends Controller
      */
     public function index()
     {
-        //
+        $bot = boton::all();
+        return view('bateriat', compact('boton'));
     }
 
     /**
@@ -22,9 +23,19 @@ class BotonController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    protected function create(array $data)
     {
-        //
+        return boton::create([
+            'Tes_id' => $data['Tes_id'],
+            'Tes_nombre' => $data['Tes_nombre'],
+            'Tes_capacidad' => $data['Tes_capacidad'],
+            'Tes_objetivo' => $data['Tes_objetivo'],
+            'Tes_desarrollo' => $data['Tes_desarrollo'],
+            'Tes_material' => $data['Tes_material'],
+            'Tes_evaluacion' => $data['Tes_evaluacion'],
+            'Tes_imagen' => $data['Tes_imagen'],
+            'Tes_icono' => $data['Tes_icono'],
+        ]);
     }
 
     /**
@@ -35,7 +46,18 @@ class BotonController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $storeData = $request->validate([
+            'Tes_id' => ['required', 'String', 'max:15'],
+            'Tes_nombre' => ['required', 'string', 'max:255'],
+            'Tes_capacidad' => ['required', 'string', 'max:255'],
+            'Tes_objetivo' => ['required', 'string', 'max:255'],
+            'Tes_desarrollo' => ['required', 'string', 'max:255'],
+            'Tes_material' => ['string', 'max:10'],
+            'Tes_evaluacion' => ['required', 'string', 'min:8', 'confirmed'],
+            'Tes_imagen' => ['string', 'max:9'],
+            'Tes_icono' => ['string', 'max:255'],
+        ]);
+        $bot = boton::create($storeData);
     }
 
     /**
