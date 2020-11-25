005f719bbc49c1bf3dd4408e05dcc707d2c55146
PaulaRomero_66 <pjromero67@misena.edu.co>
2020-11-25 17:57:46 -0500

    register

--- app/Http/Controllers/BotonController.php
+++ app/Http/Controllers/BotonController.php
@@ -48,69 +48,6 @@ class BotonController extends Controller
         //return redirect(),('status','$Boton->Tes_nombre');
     }
 
-    public function index()
-    {
-        $Boton = boton::select('Tes_id', 'Tes_nombre', 'Tes_capacidad', 'Tes_objetivo', 'Tes_desarrollo', 'Tes_material', 'Tes_evaluacion', 'Tes_imagen', 'url');
-        return view('entrenador.boton', compact('boton'));
-        //return view('entrenador.bateria');
-    }
-
-
-    // Crear un Registro (Create) 
-    public function crear()
-    {
-        $Boton = boton::all();
-        return view('entrenador.boton', compact('boton'));
-    }
-
-    // Proceso de Creación de un Registro 
-    public function store(ItemCreateRequest $request)
-    {
-        $Boton = new boton;
-        $Boton->Tes_nombre = $request->Tes_nombre;
-        $Boton->Tes_capacidad = $request->Tes_capacidad;
-        $Boton->Tes_objetivo = $request->Tes_objetivo;
-        $Boton->Tes_desarrollo = $request->Tes_desarrollo;
-        $Boton->Tes_material = $request->Tes_material;
-        $Boton->Tes_evaluacion = $request->Tes_evaluacion;
-        $Boton->Tes_imagen = $request->Tes_imagen;
-        $Boton->url = Str::slug($request->Tes_nombre, '-');  // Acá generamos la URL amigable a partir del nombre y la guardamos en la Base de Datos
-
-        $Boton->save();
-
-        $ci = $request->file('Foto');
-
-        // Validamos campos
-        $this->validate($request, [
-
-            'Nombre' => 'required',
-            'Capacidad' => 'required',
-            'Objetivo' => 'required',
-            'Desarrollo' => 'required',
-            'Material' => 'required',
-            'Evaluacion' => 'required',
-            'Foto.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
-
-        ]);
-
-        // Recibimos una o varias imágenes y las guardamos en la carpeta 'uploads'
-        foreach ($request->file('Foto') as $image) {
-            $imagen = $image->getClientOriginalName();
-            $formato = $image->getClientOriginalExtension();
-            $image->move(public_path() . '/uploads/', $imagen);
-
-            // Guardamos el nombre de la imagen en la tabla 
-            FacadesDB::table('tb_test')->insert(
-                [
-                    'formato' => $formato,
-                    'nombre' => $Boton->nombre,
-                    'created_at' => date("Y-m-d H:i:s"),
-                    'updated_at' => date("Y-m-d H:i:s")
-                ]
-            );
-        }
-    }
-
     // Leer un Registro específico (Leer)
     public function show($id)
     {
--- routes/web.php
+++ routes/web.php
@@ -56,16 +56,3 @@ Route::post('boton', 'BotonController@boto')->name('boton');
 Route::get('/ficha', 'HomeController@ficha')->name('ficha');
 Route::get('/usuario', 'HomeController@usuario')->name('usuario');
 Route::get('/entrenadores', 'HomeController@entre')->name('entrenadores');
-
-/* Rutas CRUD
- 
-/* Crear 
-Route::get('entrenador/boton/crear', 'BotonController@crear')->name('entrenador/boton/crear');
-Route::put('entrenador/boton/store', 'BotonController@store')->name('entrenador/boton/store');
- 
-/* Leer 
-Route::get('entrenador/boton', 'BotonController@index')->name('entrenador/boton');
-
-/* Vista para los detalles de un registro 
-Route::get('entrenador/boton/dbateria/{Tes_id}', ['as' => 'entrenador/boton/dbateria', 'uses' => 'BotonController@dbateria']);
-*/
