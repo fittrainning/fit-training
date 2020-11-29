333f675695316e2521fa1b80dc8a73672193aacd
Kratoz98 <ccanzola3@misena.edu.co>
2020-11-28 21:40:40 -0500

    modelos y controllers 1.0

---
+++ app/Deportistas.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Deportistas extends Model
+{
+    protected $table = 'tb_deportistas';
+
+    protected $fillable = [
+        'Dep_Usu_id','Dep_deporte','Dep_estado','Dep_razon','Dep_Cod_Fic','Dep_cod_Dee','Dep_id_Ana'
+    ];
+}
