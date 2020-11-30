573ca95fa7824bc5322b01c051ed73821c4cf7a9
Kratoz98 <ccanzola3@misena.edu.co>
2020-11-30 14:08:53 -0500

    diseño mensajeria

--- resources/views/mensajeria1.blade.php
+++ resources/views/mensajeria1.blade.php
@@ -6,15 +6,16 @@
         <div id="cont"><br><br>
             <div class="row">
                 <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
-                <div data-spy="scroll" id="color111" class="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10">
-
+                <div data-spy="scroll"  data-offset="0" id="color111" class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5">
+                    <div id="centro2"><h2>Enviados</h2></div>
                     @foreach($Mensajes as $Mensaje)
+                        @if ($Mensaje->Men_em_Usu_id == Auth::user()->Usu_id)
                         <div id="mensajes">
                             <div class="row" id="mmm">
-                                <div class="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-11">
-                                    <label>{{$Mensaje->Men_Mensaje}}</label>
+                                <div class="col-8 col-sm-8 col-md-8 col-lg-9 col-xl-10">
+                                    <label>{{$Mensaje->Men_em_Usu_id}} _ {{$Mensaje->Men_Mensaje}}</label>
                                 </div>
-                                <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-1">
+                                <div class="col-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
                                     <a href="{{ route('Mensajes.create') }}">
                                         <img src="{{ asset('img/responder.png') }}" alt="" width="25px">
                                     </a>
@@ -22,6 +23,27 @@
                                 </div>
                             </div>
                         </div>
+                        @endif
+                    @endforeach
+                </div>
+                <div data-spy="scroll" data-offset="0" id="color111" class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5">
+                    <div id="centro2"><h2>Recibidos</h2></div>
+                    @foreach($Mensajes as $Mensaje)
+                        @if ($Mensaje->Men_re_Usu_id == Auth::user()->Usu_id)
+                        <div id="mensajes">
+                            <div class="row" id="mmm">
+                                <div class="col-8 col-sm-8 col-md-8 col-lg-9 col-xl-10">
+                                    <label>{{$Mensaje->Men_em_Usu_id}} _ {{$Mensaje->Men_Mensaje}}</label>
+                                </div>
+                                <div class="col-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
+                                    <a href="{{ route('Mensajes.create') }}">
+                                        <img src="{{ asset('img/responder.png') }}" alt="" width="25px">
+                                    </a>
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
                     @endforeach
                 </div>
                 <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
