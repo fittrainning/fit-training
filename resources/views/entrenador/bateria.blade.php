61d4c672ebf6c7147a3098fc624601548b93db3f
Kratoz98 <ccanzola3@misena.edu.co>
2020-11-30 14:39:58 -0500

    bateria vista

--- resources/views/entrenador/bateria.blade.php
+++ resources/views/entrenador/bateria.blade.php
@@ -48,16 +48,22 @@
                     
                     <!--RESULTADO -->
                         <tr>
-                        
-                            <td>
-                                <a type="button" class="btn btn-secondary  btn-sm " id="buttonn" href="{{ route('dbateria') }}">
-                                    <h6><img id="lo" src="{{asset('img/clipboard.png')}}" alt="img" width="40px">&nbsp;{{ $Boton -> Tes_nombre }}</h6>
-                                </a>
-                            </td>
-                            
-                            <td>   </td>
-                            <td>   </td>
-                            <td>   </td>
+                            @if($Boton->Tes_capacidad == 'Fuerza')
+                                <td>
+                                    <a type="button" class="btn btn-secondary  btn-sm " id="buttonn" href="{{ route('dbateria') }}">
+                                        <h6><img id="lo" src="{{asset('img/clipboard.png')}}" alt="img" width="40px">&nbsp;{{ $Boton -> Tes_nombre }}</h6>
+                                    </a>
+                                </td>
+                            @endif
+                            @if($Boton->Tes_capacidad == 'Velocidad')
+                                <td>   </td>
+                            @endif
+                            @if($Boton->Tes_capacidad == 'Resistencia')
+                                <td>   </td>
+                            @endif
+                            @if($Boton->Tes_capacidad == 'Elasticidad')
+                                <td>   </td>
+                            @endif
                         </tr>
                     @endforeach
                 </table><br>
