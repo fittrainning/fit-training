795f53aa932d582b4b45a3c8feb842c204bbb329
Kratoz98 <ccanzola3@misena.edu.co>
2020-12-02 16:32:32 -0500

    mensajeria

--- resources/views/mensajeria1.blade.php
+++ resources/views/mensajeria1.blade.php
@@ -7,13 +7,13 @@
             <div class="row">
                 <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
                     @if(Session::has('menssaje'))
-                    <div class="alert alert-primary alert-dismissible fade show" role="alert">
-                        {{ Session::get('menssaje') }}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                @endif
+                        <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                            {{ Session::get('menssaje') }}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    @endif
                 </div>
             </div>
             <div class="row">
