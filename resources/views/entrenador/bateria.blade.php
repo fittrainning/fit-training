78cf97099f643bc7d70ff5aeb235c3ddd23ebe39
PaulaRomero_66 <pjromero67@misena.edu.co>
2020-12-02 21:10:45 -0500

    detalles

--- resources/views/entrenador/bateria.blade.php
+++ resources/views/entrenador/bateria.blade.php
@@ -6,78 +6,84 @@
             <div id="cont"><br><br>
 
                 <table class="table table-borderless">
-                    <tr>
-                        <th scope="col">
-                        <div class="">
-                            <div id="cap">
-                                <img id="img" src="{{ asset('../img/fuerza.png') }}"><br><br><!--Define la imagen de la capacidad-->
-                                <h5>FUERZA</h5><br> 
-                            </div>
-                        </div>
-                        </th>
-        
-                        <th scope="col">
-                        <div class="">
-                            <div id="cap">
-                                <img id="img" src="{{ asset('../img/velocidad.png') }}"><br><br>
-                                <h5>VELOCIDAD</h5><br>
-                            </div>
-                        </div> 
-                        </th>
-        
-                        <th scope="col">
-                        <div  class="">
-                            <div id="cap">
-                                <img id="img" src="{{ asset('../img/resis.png') }}"><br><br>
-                                <h5>RESISTENCIA</h5><br>
-                            </div>
-                        </div>
-                        </th>
-                        
-                        <th scope="col">
-                        <div class="">
-                            <div id="cap">
-                                <img id="img" src="{{ asset('../img/flex.png') }}"><br><br>
-                                <h5>ELASTICIDAD</h5><br>
-                            </div>
-                        </div>
-                        </th>
-                    </tr>
-                    @foreach($bateria as $Boton)
-                        
-                    
-                    <!--RESULTADO -->
+                    <thead>
                         <tr>
-                            @if($Boton->Tes_capacidad == 'Fuerza')
-                                <td>
-                                    <a type="button" class="btn btn-secondary  btn-sm " id="buttonn" href="{{ route('dbateria', [$Boton -> Tes_id] ) }}">
-                                        <h6><img id="lo" src="{{asset('img/clipboard.png')}}" alt="img" width="40px">&nbsp;{{ $Boton -> Tes_nombre }}</h6>
-                                    </a>
-                                </td>
-                            @endif
-                            @if($Boton->Tes_capacidad == 'Velocidad')
-                                <td> 
-                                    <a type="button" class="btn btn-secondary  btn-sm " id="buttonn" href="{{ route('dbateria', [$Boton -> Tes_id] ) }}">
-                                        <h6><img id="lo" src="{{asset('img/clipboard.png')}}" alt="img" width="40px">&nbsp;{{ $Boton -> Tes_nombre }}</h6>
-                                    </a>
-                                </td>
-                            @endif
-                            @if($Boton->Tes_capacidad == 'Resistencia')
-                                <td>
-                                    <a type="button" class="btn btn-secondary  btn-sm " id="buttonn" href="{{ route('dbateria', [$Boton -> Tes_id] ) }}">
-                                        <h6><img id="lo" src="{{asset('img/clipboard.png')}}" alt="img" width="40px">&nbsp;{{ $Boton -> Tes_nombre }}</h6>
-                                    </a>
-                                </td>
-                            @endif
-                            @if($Boton->Tes_capacidad == 'Elasticidad')
-                                <td>
-                                    <a type="button" class="btn btn-secondary  btn-sm " id="buttonn" href="{{ route('dbateria', [$Boton -> Tes_id] ) }}">
-                                        <h6><img id="lo" src="{{asset('img/clipboard.png')}}" alt="img" width="40px">&nbsp;{{ $Boton -> Tes_nombre }}</h6>
-                                    </a>
-                                </td>
-                            @endif
+                            <th scope="col">
+                            <div class="">
+                                <div id="cap">
+                                    <img id="img" src="{{ asset('../img/fuerza.png') }}"><br><br><!--Define la imagen de la capacidad-->
+                                    <h5>FUERZA</h5><br> 
+                                </div>
+                            </div>
+                            </th>
+            
+                            <th scope="col">
+                            <div class="">
+                                <div id="cap">
+                                    <img id="img" src="{{ asset('../img/velocidad.png') }}"><br><br>
+                                    <h5>VELOCIDAD</h5><br>
+                                </div>
+                            </div> 
+                            </th>
+            
+                            <th scope="col">
+                            <div  class="">
+                                <div id="cap">
+                                    <img id="img" src="{{ asset('../img/resis.png') }}"><br><br>
+                                    <h5>RESISTENCIA</h5><br>
+                                </div>
+                            </div>
+                            </th>
+                            
+                            <th scope="col">
+                            <div class="">
+                                <div id="cap">
+                                    <img id="img" src="{{ asset('../img/flex.png') }}"><br><br>
+                                    <h5>ELASTICIDAD</h5><br>
+                                </div>
+                            </div>
+                            </th>
                         </tr>
-                    @endforeach
+                    </thead>
+                    <tbody>
+                        @foreach($bateria as $Boton)
+                         
+                        <!--RESULTADO -->
+                            <tr>
+                                @if($Boton->Tes_capacidad == 'Fuerza')
+                                    <td>
+                                        <a type="button" class="btn btn-secondary  btn-sm " id="buttonn" href="{{ route('dbateria', [$Boton -> Tes_id] ) }}">
+                                            <h6><img id="lo" src="{{asset('img/clipboard.png')}}" alt="img" width="40px">&nbsp;{{ $Boton -> Tes_nombre }}</h6>
+                                        </a>
+                                    </td>
+                                @endif
+                                @if($Boton->Tes_capacidad == 'Velocidad')
+                                <td></td>
+                                    <td> 
+                                        <a type="button" class="btn btn-secondary  btn-sm " id="buttonn" href="{{ route('dbateria', [$Boton -> Tes_id] ) }}">
+                                            <h6><img id="lo" src="{{asset('img/clipboard.png')}}" alt="img" width="40px">&nbsp;{{ $Boton -> Tes_nombre }}</h6>
+                                        </a>
+                                    </td>
+                                @endif
+                                @if($Boton->Tes_capacidad == 'Resistencia')
+                                <td></td><td></td>
+                                    <td>
+                                        <a type="button" class="btn btn-secondary  btn-sm " id="buttonn" href="{{ route('dbateria', [$Boton -> Tes_id] ) }}">
+                                            <h6><img id="lo" src="{{asset('img/clipboard.png')}}" alt="img" width="40px">&nbsp;{{ $Boton -> Tes_nombre }}</h6>
+                                        </a>
+                                    </td>
+                                @endif
+                                @if($Boton->Tes_capacidad == 'Elasticidad')
+                                <td></td><td></td><td></td>
+                                    <td>
+                                        <a type="button" class="btn btn-secondary  btn-sm " id="buttonn" href="{{ route('dbateria', [$Boton -> Tes_id] ) }}">
+                                            <h6><img id="lo" src="{{asset('img/clipboard.png')}}" alt="img" width="40px">&nbsp;{{ $Boton -> Tes_nombre }}</h6>
+                                        </a>
+                                    </td>
+                                @endif
+                            </tr>
+                        @endforeach
+                    </tbody>
                 </table><br>
                 
                 <a href="{{ route('boton') }}">
