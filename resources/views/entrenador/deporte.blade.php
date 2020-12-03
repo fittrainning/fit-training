6d0ee79e5401909a6ea0f3229f7a5b1365d61850
Kratoz98 <ccanzola3@misena.edu.co>
2020-12-03 16:45:18 -0500

    vista deportes 1

--- resources/views/entrenador/deporte.blade.php
+++ resources/views/entrenador/deporte.blade.php
@@ -17,7 +17,7 @@
                         <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
                         <div class="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10">
                             <form action="">
-                                in
+                                
                             </form>
                         </div>
                         <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
