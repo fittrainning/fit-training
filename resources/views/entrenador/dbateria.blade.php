e4225e35a2f3a7f7476706d53e73b8db105d731c
PaulaRomero_66 <pjromero67@misena.edu.co>
2020-12-02 21:36:15 -0500

    detalles

--- resources/views/entrenador/dbateria.blade.php
+++ resources/views/entrenador/dbateria.blade.php
@@ -6,8 +6,7 @@
             <div id="cont"><br><br>
                 <div id="fondo">
                     <div class="row">
-                        <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
-                        </div>
+                        <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4"></div>
                         <div  class="col-10"></div>
                         <div  class="col-2 ">
                             <div id="cerrar">
@@ -36,21 +35,23 @@
                                         <strong>OBJETIVO</strong>
                                         <br>{{ $Boton->Tes_objetivo}}
                                         <br><br>
+
                                         <strong>DESARROLLO:</strong>
                                         <br>{{ $Boton->Tes_desarrollo }}
                                         <br><br>
+
                                         <strong>MATERIAL:</strong>
                                         <br>{{ $Boton->Tes_material }}
                                         <br><br>
+
                                         <strong>EVALUACION:</strong>
                                         <br>{{ $Boton->Tes_evaluacion }}
                                         <br><br>
+
                                         <strong>IMAGEN:</strong><br>
                                         <br><img src="/uploads/{{ $Boton->Tes_imagen}}">
-                                        <br><br>
+                                        <br><br><br><br>
 
-                                        
-                                        <br><br>
                                     </div>
                                     
                                     <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
