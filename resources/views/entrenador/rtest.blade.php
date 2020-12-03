23c657fa33f0150640c1a97e5102ec272407ea8d
PaulaRomero_66 <pjromero67@misena.edu.co>
2020-12-03 18:26:54 -0500

    campos

--- resources/views/entrenador/rtest.blade.php
+++ resources/views/entrenador/rtest.blade.php
@@ -8,7 +8,34 @@
                     <div class="row">
                         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
                             <div id="color" class="content-box-large">
+                                <!-- DOCUMENTO -->
+                                <div class="form-group row">
+                                    <label for="Usu_id" class="col-md-4 col-form-label text-md-right">{{ __('Numero de Documento*') }}</label>
 
+                                    <div id="let" class="col-md-6">
+                                        <input id="Usu_id" type="number" class="form-control @error('Usu_id') is-invalid @enderror" name="Usu_id" value="{{ old('Usu_id') }}" required autocomplete="Usu_id" autofocus>
+                                        @error('Usu_id')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <!-- FECHA -->
+                                <div class="form-group row">
+                                    <label for="Usu_fecha_nacimiento" class="col-md-4 col-form-label text-md-right">{{ __('Fecha*') }}</label>
+
+                                    <div id="let" class="col-md-6">
+                                        <input id="Usu_fecha_nacimiento" type="date" class="form-control @error('Usu_fecha_nacimiento') is-invalid @enderror" name="Usu_fecha_nacimiento" value="{{ old('Usu_fecha_nacimiento') }}" required autocomplete="Usu_fecha_nacimiento" autofocus>
+
+                                        @error('Usu_fecha_nacimiento')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
