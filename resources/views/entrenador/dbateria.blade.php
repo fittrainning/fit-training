29b6187968f72b30fb8d93db696df7872c5c1f77
Kratoz98 <ccanzola3@misena.edu.co>
2020-12-02 22:57:30 -0500

    diseño detalle de bateria

--- resources/views/entrenador/dbateria.blade.php
+++ resources/views/entrenador/dbateria.blade.php
@@ -6,57 +6,61 @@
             <div id="cont"><br><br>
                 <div id="fondo">
                     <div class="row">
-                        <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4"></div>
-                        <div  class="col-10"></div>
-                        <div  class="col-2 ">
-                            <div id="cerrar">
-                                <input OnClick="location.href='{{ url('/bateria') }}'" type=image src="{{asset ('img/cerrar.png ')}}" width="30" height="30"><!--input boton de cerrar y regresar a index-->
-                            </div>
-                        </div>
-                    </div><br><br>
-                    <div class="row">
                         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
                             <div id="color" class="content-box-large">
                                 @foreach($bateri as $Boton)
-                                    
-                                
-                                @if(!empty($Boton->Tes_id)) 
-                                <div class="row">
-                                    <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
-                                    <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8" id="centro">
-                                        <h2>{{ $Boton->Tes_nombre }}</h2>
-                                    </div>
-                                    <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
-                                   
-                                    <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
-                                        <strong>OBJETIVO:</strong>
-                                        <br>{{ $Boton->Tes_objetivo}}
-                                        <br><br>
-
-                                        <strong>DESARROLLO:</strong>
-                                        <br>{{ $Boton->Tes_desarrollo }}
-                                        <br><br>
-
-                                        <strong>MATERIAL:</strong>
-                                        <br>{{ $Boton->Tes_material }}
-                                        <br><br>
-
-                                        <strong>EVALUACION:</strong>
-                                        <br>{{ $Boton->Tes_evaluacion }}
-                                        <br><br>
-
-                                        <strong>IMAGEN:</strong><br>
-                                        <br><img src="/uploads/{{ $Boton->Tes_imagen}}">
-                                        <br><br><br><br>
-
-                                    </div>
-                                    
-                                    <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
-                                </div>
-                                @endif
+                                    @if(!empty($Boton->Tes_id)) 
+                                        <div class="row">
+                                            <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
+                                            <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8" id="centro">
+                                                <br>
+                                                <h2>{{ $Boton->Tes_nombre }}</h2>
+                                            </div>
+                                            <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+                                                <div id="cerrar">
+                                                    <input OnClick="location.href='{{ url('/bateria') }}'" type=image src="{{asset ('img/cerrar.png ')}}" width="30" height="30"><!--input boton de cerrar y regresar a index-->
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
+                                        
+                                            <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
+                                                <div class="row" id="let4">
+                                                    <h4>OBJETIVO</h4>
+                                                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                                                        {{ $Boton->Tes_objetivo}}
+                                                    </div>
+                                                </div><br>
+                                                <div class="row" id="let4">
+                                                    <h4>DESARROLLO:</h4>
+                                                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                                                        {{ $Boton->Tes_desarrollo }}
+                                                    </div>
+                                                </div><br>
+                                                <div class="row" id="let4">
+                                                    <h4>MATERIAL:</h4>
+                                                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                                                        {{ $Boton->Tes_material }}
+                                                    </div>
+                                                </div><br>
+                                                <div class="row" id="let4">
+                                                    <h4>EVALUACION:</h4>
+                                                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                                                        {{ $Boton->Tes_evaluacion }}
+                                                    </div>
+                                                </div><br>
+                                                <div class="row" id="let4">
+                                                    <h4>IMAGEN:</h4>
+                                                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                                                        <img src="/uploads/{{ $Boton->Tes_imagen}}">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
+                                        </div>
+                                    @endif
                                 @endforeach
                             </div>
                         </div>
