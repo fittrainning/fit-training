71248dc54a373f243d59bdbcd1f78ae747ea3068
Kratoz98 <ccanzola3@misena.edu.co>
2020-12-04 12:49:30 -0500

    anamnesis controlador 1

--- app/Http/Controllers/AnamnesisController.php
+++ app/Http/Controllers/AnamnesisController.php
@@ -14,7 +14,7 @@ class AnamnesisController extends Controller
      */
     public function index()
     {
-        //
+        return view("deportistas.Anamnesis_3", ["Ananmnesis"=>Anamnesis::all()]);
     }
 
     /**
