61e5bb5ef8a338f3d34ead11eec36a338c4c4f0e
Kratoz98 <ccanzola3@misena.edu.co>
2020-12-07 19:39:45 -0500

    base de datos 5

--- database/migrations/2020_08_15_003720_create_tb_planentrenamiento_table.php
+++ database/migrations/2020_08_15_003720_create_tb_planentrenamiento_table.php
@@ -18,10 +18,6 @@ class CreateTbPlanentrenamientoTable extends Migration
             $table->date('Pla_fecini');
             $table->date('Pla_fecfin');
             $table->string('Pla_deporte');
-            $table->string('Pla_res_fuerza');
-            $table->string('Pla_res_resistencia');
-            $table->string('Pla_res_velocidad');
-            $table->string('Pla_res_flexibilidad');
             $table->string('Pla_id_Ent')->references('Ent_Usu_id')->on('tb_entrenador')->onUpdate('cascade');
             $table->string('Pla_id_Dep')->references('Dep_Usu_id')->on('tb_deportistas')->onUpdate('cascade');
             $table->timestamps();
