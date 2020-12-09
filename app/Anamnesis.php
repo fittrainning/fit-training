0d607506ee982863ac570037cda3c09d23d82185
Kratoz98 <ccanzola3@misena.edu.co>
2020-12-09 08:40:03 -0500

    anamnesis

--- app/Anamnesis.php
+++ app/Anamnesis.php
@@ -9,6 +9,6 @@ class Anamnesis extends Model
     protected $table = 'tb_anamnesis';
 
     protected $fillable = [
-        'Ana_id','Ana_DepPract','Ana_depo','Ana_Logros','Ana_Logrosdeportivos','Ana_HistorialDeportivo','Ana_HistorialMedico','Ana_HabitosAlimenticios'
+        'Ana_id','Ana_Dep_id','Ana_DepPract','Ana_depo','Ana_Logros','Ana_Logrosdeportivos','Ana_HistorialDeportivo','Ana_HistorialMedico','Ana_HabitosAlimenticios'
     ];
 }
