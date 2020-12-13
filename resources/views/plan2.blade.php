13b397f1d6d4baa4c9f7257e54f7f174e4926455
Kratoz98 <ccanzola3@misena.edu.co>
2020-12-13 11:45:39 -0500

    plan de entrenamiento 3.8

--- resources/views/plan2.blade.php
+++ resources/views/plan2.blade.php
@@ -6,24 +6,25 @@
             <div id="cont"><br><br>
                 <div class="row">
                     <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                        <form action="{{ route('Planes.update', $plan->Pla_id) }}" method="POST">
-                            @method("PUT")
+                        <form action="{{ route("Planes.update", [$plan]) }}" method="POST">
                             @csrf
+                            @method("PUT")
                             <div class="row">
                                 <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" id="colo">
                                     <h2 id="centro">Plan de Entrenamiento</h2>
                                     <hr id="separa"><br>
                                     <div class="row">
+                                        <input type="text" name="Pla_id" value="{{ $plan->Pla_id }}">
                                         <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" id="centro3">
                                             <div id="let3">
                                                 Fecha Inicial
-                                                <input type="date" name="Pla_fecini">
+                                                <input type="text" name="Pla_fecini" value="">
                                             </div>
                                         </div>
                                         <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" id="centro3">
                                             <div id="let3">
                                                 Fecha Final
-                                                <input type="date" name="Pla_fecfin">
+                                                <input type="text" name="Pla_fecfin" value="">
                                             </div>
                                         </div>
                                     </div>
@@ -32,13 +33,13 @@
                                         <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" id="centro3">
                                             <div id="let3">
                                                 Deportista
-                                                <input type="text" name="Pla_id_Dep" value="{{$plan->Pla_id_Dep}}">
+                                                <input type="text" name="Pla_id_Dep" value="">
                                             </div>
                                         </div>
                                         <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" id="centro3">
                                             <div id="let3">
                                                 Deporte
-                                                <input type="text" name="Pla_deporte" value="{{ $plan->Pla_deporte }}">
+                                                <input type="text" name="Pla_deporte" value="">
                                             </div>
                                         </div>
                                         <input type="hidden" name="Pla_id_Ent" value="{{ Auth::user()->Usu_id }}">
